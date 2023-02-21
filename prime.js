c6120db0ab3388eed3d70c539c5ea5491042e224
pranaytandel09 <pranaytandel09@gmail.com>
2023-02-21 18:38:52 +0530

    Problem to check a no is prime or not.

---
+++ prime.js
@@ -0,0 +1,13 @@
+let number=15;
+let count=0;
+for(let i=2; i<=Math.floor(number/2); i++){
+if(number%i==0){
+count++;
+}
+}
+if(count==0){
+console.log("Yes, it is a prime number")
+}
+else{
+console.log("it is not a prime number")
+}
