588b64c32ab7be312fb5d6c4bd3b1618e5813ad7
pranaytandel09 <pranaytandel09@gmail.com>
2023-02-21 20:21:30 +0530

    made changes

--- prime.js
+++ prime.js
@@ -1,13 +1,13 @@
 let number=15;
 let count=0;
-for(let i=2; i<=Math.floor(number/2); i++){
+for(let i=1; i<=number; i++){
 if(number%i==0){
 count++;
 }
 }
-if(count==0){
-console.log("Yes, it is a prime number")
+if(count==1){
+console.log("Yes")
 }
 else{
-console.log("it is not a prime number")
+console.log("No")
 }
